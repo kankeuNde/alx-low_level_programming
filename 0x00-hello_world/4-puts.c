22921d2660c9bd458d10113a06e4dd4fd8215359
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-10 21:00:18 -0700

    make the code betty compatible

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main(){
+
+/**
+ *print a message using puts function
+*/
+int main(void)
+{
 	char msg[] = "Programming is like building a multilingual puzzle";
 	puts(msg);
-	return 0;
+	return (0);
 }
