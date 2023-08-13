55dbe15aa569cfaebef07647d0025b6850604e2f
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-13 01:42:09 -0700

    definition and separation of init of variable last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,15 +6,16 @@
 /**
  * main - Entry point
  *
- * Return - Always (0)
+ * Return - Always 0 (success
  */
 int main(void)
 {
 	int n;
-	
+	int last_digit;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
+	last_digit = n % 10;
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
 	else if (last_digit == 0)
