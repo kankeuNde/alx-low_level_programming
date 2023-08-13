1fec6eb0b49196b6bb7c9604762dcd95cd9f62a3
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-13 11:14:03 -0700

    betty styling

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - entry point of the code
  *
- * Return - 0 Successful
+ * Return: Always 0( Success)
  */
-
 int main(void)
 {
 	char msg[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(msg);
 	return (0);
 }
