ff45053311d0f8137f3bf633bd359198e8344237
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-13 01:49:10 -0700

    adding new line at the end of each line

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes here*/
 
 /**
  * main - Entry point
  *
- * Return - Always 0 (success
+ * Return - Always 0 (success)
  */
 int main(void)
 {
@@ -17,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	if (last_digit > 5)
-		printf("Last digit of %d is %d and is greater than 5", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit == 0)
-		printf("Last digit of %d is %d and is 0", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	return (0);
 }
