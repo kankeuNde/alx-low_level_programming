0643df9c81ae596d2dd1b344530d48d18aa3767c
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-13 18:59:51 -0700

    print comma and spaces conditionally

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,11 @@ int main(void)
 			{
 				putchar(i);
 				putchar(j);
-				putchar(',');
-				putchar(' ');
+				if (i != '8' && j != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	putchar('\n');
