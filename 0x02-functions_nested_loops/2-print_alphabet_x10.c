9e4665d1ef64cd893e3915aa0fc3bd1d8ee5de30
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-15 09:37:02 -0700

    remove the import of functions totally

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_alphabet_x10() - check the code
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i<10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 			putchar(j);
