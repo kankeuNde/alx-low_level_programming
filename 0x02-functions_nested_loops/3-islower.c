368a08eab1cccef367684ee0cf048011a0bad722
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-16 13:40:56 -0700

    add description to the function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * islower(int c) - checks if c is lowercase or uppercase
+ * _islower(int c) - checks if c is lowercase or uppercase
  *
  * Return: 0 if c is lowercase, 1 otherwise
  */
