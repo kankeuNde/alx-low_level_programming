4631a7efa776b1e0a635ce42780e094456487d5b
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-15 20:56:14 -0700

    test is c is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+/**
+ * isalpha(int c) - checks if c is lowercase or uppercase
+ *
+ * Return: 1 if c is a letter, 1 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90)||(c >= 97 && c <= 122))
+		return 1;
+	else
+		return 0;
+}
