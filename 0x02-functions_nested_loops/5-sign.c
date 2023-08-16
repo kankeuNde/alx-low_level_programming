85d0950dbe81a45742fd2da80d3ba0839730a48a
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-16 15:09:10 -0700

    Betty clean up

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * islower(int c) - checks if c is lowercase or uppercase
- *
- * Return: 0 if c is lowercase, 1 otherwise
+ * print_sign - checks if c is lowercase or uppercase
+ * @c: input integer
+ * Return: 1 if c > 0 , 0 if c equal to 0 and -1 if c < 0
  */
 int print_sign(int c)
 {
 	if (c > 0)
 	{
 		putchar('+');
-		return 1;
-	}else if (c ==0)
+		return (1);
+	}
+	else if (c == 0)
 	{
 		putchar('0');
-		return 0;
-	}else
+		return (0);
+	}
+	else
 	{
 		putchar('-1');
-		return -1;
+		return (-1);
 	}
 }
