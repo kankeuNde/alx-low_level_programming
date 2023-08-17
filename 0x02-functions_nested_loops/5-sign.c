2146291671b0a989a45042709d05d36e376b07ae
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-16 18:34:42 -0700

    Print using _putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (c == 0)
 	{
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
+		_putchar('-');
 		return (-1);
 	}
 }
