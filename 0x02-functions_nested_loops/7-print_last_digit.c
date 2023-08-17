c03a3c2f288a2c850e7eb086528950ba99461d92
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-16 19:07:45 -0700

    put a char before returning it

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - compute the last digit of a number
- *@c: input integer
+ * @c: input integer
 
  * Return: n the last digit of a number
  */
@@ -11,7 +11,9 @@ int print_last_digit(int c)
 	int l_digit;
 
 	l_digit = c % 10;
-
+	if (l_digit < 0)
+		l_digit = l_digit * -1;
+	_putchar(l_digit + '0');
 	return (l_digit);
 }
 
