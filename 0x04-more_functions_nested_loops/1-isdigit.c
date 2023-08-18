00275baebcfc85fdc12550e1bb1b1b3acc146c7a
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-17 22:47:43 -0700

    function that checks for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - determine if it is a number
+ * @c: integer value of the character
+ * 
+ * Return:1 if c is a digit, 0 otherwise
+ */
+ int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0-function that checks for uppercase character
+1-function that checks for a digit
