a35a66a7f62f560310f41dd02bb9d1c4c5ea074d
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-18 10:39:09 -0700

    print a square using #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square using the # signs
+ * @size: n=m= size of the square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int line, col;
+
+	if (size > 0)
+		for (line = 0; line < size; line++)
+			for (col = 0; col < size; col++)
+				_putchar('#');
+}
