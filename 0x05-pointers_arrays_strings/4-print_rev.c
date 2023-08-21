7306c927a0134083e148a49ec2fa9c00fb09f7c5
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-21 16:40:30 -0700

    add the _strlen function

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,3 +15,20 @@ void print_rev(char *s)
 		_putchar(s[i]);
 	_putchar('\n');
 }
+
+/**
+ * _strlen - compute the length of a string
+ * @s: string to compute
+ * Return: returns the lenght of s
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
