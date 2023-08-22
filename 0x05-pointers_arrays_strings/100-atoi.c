61b543a3943e0cb44e2cb3c12ed7062dac837087
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-21 19:46:18 -0700

    betty styling

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,15 +14,13 @@ int _atoi(char *s)
 	i = 1;
 	num = 0;
 
-	do 
-	{
+	do {
 		if (*s == '-')
 			i *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
-	return (num *i);
+	} while (*s++);
+	return (num * i);
 }
