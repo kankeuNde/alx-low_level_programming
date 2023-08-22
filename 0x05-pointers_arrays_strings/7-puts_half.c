3eb0444ab961ae74bf7dcfc00b7c478db3c5163f
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-21 18:36:55 -0700

    print the half part of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - print the seconf half part of a string
+ * @str: input string
+ * Return: noting
+ */
+void puts_half(char *str)
+{
+	int len;
+	int i;
+
+	len = _strlen(str);
+	
+	if (len % 2 == 0)
+		i = len / 2;
+	else
+		i = (len + 1) / 2;
+	while (str[i] != '\0')
+		_putchar(str[i++]);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - compute the length of a string
+ * @s: string to compute
+ * Return: returns the lenght of s
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
