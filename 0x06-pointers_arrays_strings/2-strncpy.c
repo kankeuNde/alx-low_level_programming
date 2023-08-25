51c878cceb781cabc2378d14f6f15e6e456e64fa
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-25 00:28:52 -0700

    copy string from src to dest of length n

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string from src to dest
+ * @dest: input destination string
+ * @src: input string to be copied
+ * @n: input n number of bytes to copy
+ *
+ * Return: string copied
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *d = dest;
+	char *s = src;
+	int i = 0;
+	
+	while (i < n && *d != '\0')
+	{
+		*d = *s;
+		d++;
+		s++;
+		n--;
+	}
+	for (; i < n; i++)
+		*d++ = '\0';
+	return (dest);
+}
