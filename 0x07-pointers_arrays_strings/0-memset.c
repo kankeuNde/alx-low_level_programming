655d43c8ce9d7b58b39624bf35b540401b93b68f
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-28 13:14:50 -0700

    set a constant value in the memory

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: input pointer to the memory to set constant
+ * @b: input char to set
+ * @n: input number of elements to set starting from the pointer zone
+ *
+ * Return: pointer to memory area s;
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+More pointers arrays and strings
+1 - function that fills with a constant byte.
