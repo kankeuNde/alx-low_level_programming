1f22ce81c101450d4815a79cd637409fe659889b
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-08-28 19:47:00 -0700

    find the first non occurrence of the character from string2 to string1

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strspn - get the length of a prefix substring
+ * @s: input string
+ * @accept: input bytes to search from s
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+	int i;
+	char *string1;
+	char *string2;
+
+	n = 0;
+	string1 = s;
+	string2 = accept;
+
+	while (*string1)
+	{
+		for (i = 0; string2[i]; i++)
+		{
+			if (string2[i] == *s)
+			{
+				n++;
+				break;
+			}
+			else if ((string2[i + 1]) == '\0')
+				return (n);
+		}
+		string1++;
+	}
+	return (n);
+}
