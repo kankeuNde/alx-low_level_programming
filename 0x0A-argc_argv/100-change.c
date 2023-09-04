297175ddb939ebf855d8553bf4e117d91d31f218
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-03 22:01:48 -0700

    Betty styling

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,37 +19,35 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argv[1]) < 0)
+	if (atoi(argv[1]) > 0)
 	{
-		printf("0\n");
+		cents = atoi(argv[1]);
+		while (cents >= 25)
+		{
+			cents -= 25;
+			coins++;
+		}
+		while (cents >= 10)
+		{
+			cents -= 10;
+			coins++;
+		}
+		while (cents >= 5)
+		{
+			cents -= 5;
+			coins++;
+		}
+		while (cents >= 2)
+		{
+			cents -= 2;
+			coins++;
+		}
+		while (cents >= 1)
+		{
+			cents -= 1;
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (0);
 	}
-
-	cents = atoi(argv[1]);
-	while (cents >= 25)
-	{
-		cents -= 25;
-		coins++;
-	}
-	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
-	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
-	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
-	while (cents >= 1)
-	{
-		cents -= 1;
-		coins++;
-	}
-	printf("%d\n", coins);
-	return (0);
 }
