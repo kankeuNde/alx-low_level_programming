e392861efa63c486ed430ea6867dd4cac30b0475
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-03 21:20:43 -0700

    filter non digits numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,18 +11,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, res;
+	int i, res, j;
 
 	res = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) > 0)
-			res += atoi(argv[i]);
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		res += atoi(argv[i]);
 	}
 	printf("%d\n", res);
 	return (0);
