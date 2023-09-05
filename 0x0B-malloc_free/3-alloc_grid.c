914961a0dbadcaf06181a65850c58635b5bba90e
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-05 10:30:59 -0700

    free memory in case of failure

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,8 +16,17 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	array = (int **)malloc(width * sizeof(int *));
+	if (array = NULL)
+		return (NULL);
 	for (i = 0; i < width; i++)
+	{
 		array[i] = (int *)malloc(height * sizeof(int));
+		if (array[i] == NULL)
+		{
+			free(array);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < width; i++)
 		for (j = 0; j < height; j++)
 			array[i][j] = 0;
