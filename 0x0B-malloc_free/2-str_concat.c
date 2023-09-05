440aa515d046c362d3dd7a4cc5eba0d8beb2745d
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-04 22:00:30 -0700

    Concatenate two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+int _strlen(char *s);
+
+/**
+ * str_concat - concatenate two strings s1 and s2
+ *
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: pointer to the new concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, len, i, j;
+	char *str;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+	j = 0;
+	for (i = len1; (i < len - 1) && (j < len2); i++)
+	{
+		str[i] = s2[j];
+		j++;
+	}
+	str[len - 1] = '\0';
+	return (str);
+}
+
+/**
+ * _strlen - calculate the size of a string
+ *
+ * @s: input string
+ *
+ * Return: integer size of s
+ */
+int _strlen(char *s)
+{
+	int i, len;
+
+	len = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+	return (len);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 Memory allocation and deallocation
 1 - function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as parameter
+2 - function that concatenates two strings
