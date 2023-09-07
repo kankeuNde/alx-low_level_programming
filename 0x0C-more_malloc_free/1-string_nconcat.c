5ee4165086c21acbcac7a59e6a96ffb9690ece2e
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-07 13:50:45 -0700

    manage NULL NULL case

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,24 +16,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	len1 = 0;
 	len2 = 0;
-	i = 0;
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
 			len1++;
 	if (s2 != NULL)
 		for (i = 0; s2[i] != '\0'; i++)
 			len2++;
+	i = 0;
 	len = len1 + n + 1;
 	str = malloc(len * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-
-	for (i = 0; (s1[i] != '\0') && (i < len1); i++)
-	{
-		str[i] = s1[i];
-	}
-	for (j = 0; (s2[j] != '\0' && j < n && j < len2); j++)
-		str[i++] = s2[j];
+	if (s1 != NULL)
+		for (i = 0; (s1[i] != '\0') && (i < len1); i++)
+			str[i] = s1[i];
+	if (s2 != NULL)
+		for (j = 0; (s2[j] != '\0' && j < n && j < len2); j++)
+			str[i++] = s2[j];
 	str[i] = '\0';
 	return (str);
 }
