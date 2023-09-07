93cd8a9a1283611da03577965b978fa80c4915ad
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-07 12:13:19 -0700

    concatenate two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenate two strings
+ *
+ * @s1: input string
+ * @s2: input string
+ * @n: input n number of characters to copy
+ *
+ * Return: returns the pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, j, len1, len2, len;
+
+	len1 = 0;
+	len2 = 0;
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			len1++;
+	if (s2 != NULL)
+		for (i = 0; s2[i] != '\0'; i++)
+			len2++;
+	len = len1 + n + 1;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; (s1[i] != '\0') && (i < len1); i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; (s2[j] != '\0' && j < n && j < len2); j++)
+		str[i++] = s2[j];
+	str[i] = '\0';
+	return (str);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,2 +1,3 @@
 void *malloc_checked(unsigned int b);
 #include <stdlib.h>
+char *string_nconcat(char *s1, char *s2, unsigned int n);
