166cb630533d95128d86e626ac0573b7aead0dec
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-08 11:12:29 -0700

    return a void pointer from a char pointer

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ *
+ * @nmemb: number of elements
+ * @size: size of each element
+ *
+ * Return: returns a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+	return (arr);
+}
