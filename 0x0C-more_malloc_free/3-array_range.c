9009145d5ee8d1c1950511ebec5f18a4d11fb5c6
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-10 12:41:27 -0700

    return NULL if malloc return NULL

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,6 +17,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	len = max - min + 1;
 	arr = malloc(len * sizeof(int));
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < (len * sizeof(int)) && min <= max; i++)
 		arr[i] = min++;
 	return (arr);
