c9ce4e4751c02440078e342ae8cb5c8a5d35c9f3
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-13 15:08:53 -0700

    find integer in an array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - search for an integer
+ * @array: input array
+ * @size: size of the array
+ * @cmp: function pointer
+ *
+ * Return: -1 if no match or n if match
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) != 0)
+			return (i);
+	return (-1);
+}
