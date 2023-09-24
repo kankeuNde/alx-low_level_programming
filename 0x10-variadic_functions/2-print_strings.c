8e289bd74a519720294056c5009bb5891b28b8f9
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-24 04:24:25 -0700

    print nil in case null is the string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", separator);
 		}
 		str = va_arg(ap, char *);
-		printf("%s", str);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		va_end(ap);
 	}
 	printf("\n");
