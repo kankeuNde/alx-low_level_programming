e72433bad759d957dbd46b5d8affc6d205617229
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-09-24 03:51:44 -0700

    proceed if and only if n is greater than 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,15 +12,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ap;
 	unsigned int i;
 
-	va_start(ap, n); /* Initialize the argument list.*/
-
-	for (i = 0; i < n - 1; i++)
-	{
-		printf("%d", va_arg(ap, int));
-		if (separator != NULL)
-			printf("%s", separator);
-	}
 	if (n > 0)
+	{
+		va_start(ap, n); /* Initialize the argument list.*/
+
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d", va_arg(ap, int));
+			if (separator != NULL)
+				printf("%s", separator);
+		}
 		printf("%d\n", va_arg(ap, int));
-	va_end(ap);
+		va_end(ap);
+	}
 }
