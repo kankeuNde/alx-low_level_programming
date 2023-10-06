dda4859c2cd14f36fa22f1fc97025810e0a4d33b
Rodrigue Kankeu <rodriguekankeu1@gmail.com>
2023-10-05 19:49:45 -0700

    open, read and write from a file

---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+0 - ssize_t read_textfile - function that reads a text file and prints it to the POSIX standard output
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
