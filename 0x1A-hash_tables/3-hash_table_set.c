b699e0b79bff2a3eac8b6687082d0b59a806791d
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-22 11:01:11 -0800

    Add element in the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,20 +14,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
 	size = ht->size;
 	index = key_index((unsigned const char *)key, size);
 
+	/*collision mgnt*/
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+
+	/* New node initialization*/
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 
-	node->key = (char *)key;
-	node->value = (char *)value;
-	node->next = NULL;
-	if (ht->array[index] != NULL)
-	{
-		node->next = ht->array[index]->next;
-	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
 	ht->array[index] = node;
 	return (1);
 }
