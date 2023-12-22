2fb1d7cb12aaf2d42bd1fcaca25efb2854966bd9
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-22 08:17:03 -0800

    structure member init

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
-	hash_table = malloc(size * sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
+
 	if (hash_table == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
+
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 	return (hash_table);
 }
